0f7878e2747da7bef12d49b1375c283bf1f54390
Aimin Yan <aiminy@zeabase.sgn.cornell.edu>
2014-07-07 11:14:08 -0400

    add loading SNP data for one accession

--- code/code_bash_shell/Load_Single_SNPFromGBS2zeabase2.sh
+++ code/code_bash_shell/Load_Single_SNPFromGBS2zeabase2.sh
@@ -6,11 +6,11 @@ echo "$pop"
 
 cn=`sed -n 2,2p "$snp_file" | tr "\t" "\n" | wc -l`
  
-cn=8
+#cn=8
 
-for (( i=8; i <= $cn; i++ ))
+for (( i=9; i <= $cn; i++ ))
 do
 #echo "$i"
-perl $HOME/code/code_perl/load_zeabase_snps.pl -H localhost -D zea -i <(cut -f2,$i "$snp_file") -p GBS_Cornell_Biotech -g "$pop" < $HOME/DataFromVahid/Data4EachTrialAfterQC/Data4EachTrialAfterQCNew/database_user_name_pd2.txt
+perl $HOME/code/code_perl/load_zeabase_snps.pl -H db3.sgn.cornell.edu -D zea -i <(cut -f2,$i "$snp_file") -p GBS_Cornell_Biotech -g "$pop" < $HOME/code/database_user_name_pd2.txt
 
 done
